189229b5b815e6e12d312418f93ea5f7cba13ad2
SickoCoder <developer.tony17@gmail.com>
2020-09-18 22:51:51 +0100

    feat: project proposal

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Form, Col, Button, Alert } from 'react-bootstrap';
 
@@ -23,69 +24,75 @@ const Login = () => {
   };
 
   return (
-    <Container>
-      <LoginWrapper>
-        <div
-          style={{
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: '2rem',
-          }}
-        >
-          <img
-            src="/svg/logo_blue.svg"
-            alt="logo blue"
-            style={{ width: '60%' }}
-          />
-        </div>
-        {error && <Alert variant="danger">Credencias erradas!</Alert>}
-        <Form onSubmit={handleLogin}>
-          <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Control
-                type="text"
-                placeholder="Nome de usuario"
-                onChange={(event) => setUser(event.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col}>
-              <Form.Control
-                type="password"
-                placeholder="Palavra Passe"
-                onChange={(event) => setPass(event.target.value)}
-              />
-            </Form.Group>
-          </Form.Row>
-          <Form.Row
+    <>
+      <Head>
+        <title>SIRA - Entrar</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <LoginWrapper>
+          <div
             style={{
+              width: '100%',
               display: 'flex',
               justifyContent: 'center',
-              marginTop: '2rem',
+              alignItems: 'center',
+              marginBottom: '2rem',
             }}
           >
-            <Button type="submit" style={{ minWidth: '12rem' }}>
-              ENTRAR
-            </Button>
-          </Form.Row>
-        </Form>
-      </LoginWrapper>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          marginTop: '1rem',
-          color: '#fff',
-          textDecoration: 'underline',
-          cursor: 'pointer',
-        }}
-      >
-        Esqueci a palavra passe
-      </div>
-    </Container>
+            <img
+              src="/svg/logo_blue.svg"
+              alt="logo blue"
+              style={{ width: '60%' }}
+            />
+          </div>
+          {error && <Alert variant="danger">Credencias erradas!</Alert>}
+          <Form onSubmit={handleLogin}>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="text"
+                  placeholder="Nome de usuario"
+                  onChange={(event) => setUser(event.target.value)}
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Control
+                  type="password"
+                  placeholder="Palavra Passe"
+                  onChange={(event) => setPass(event.target.value)}
+                />
+              </Form.Group>
+            </Form.Row>
+            <Form.Row
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                marginTop: '2rem',
+              }}
+            >
+              <Button type="submit" style={{ minWidth: '12rem' }}>
+                ENTRAR
+              </Button>
+            </Form.Row>
+          </Form>
+        </LoginWrapper>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '1rem',
+            color: '#fff',
+            textDecoration: 'underline',
+            cursor: 'pointer',
+          }}
+        >
+          Esqueci a palavra passe
+        </div>
+      </Container>
+    </>
   );
 };
 
